f959c12a61309a9e58c1e5d0e1551b241896df61
premit01 <habibur.premit01@gmail.com>
2023-10-07 10:12:25 +0600

    chage login and singin page left side

--- src/app/(authentication)/signup/page.js
+++ src/app/(authentication)/signup/page.js
@@ -1,17 +1,21 @@
 import SignUpForm from '@/components/authentication/SignUpForm/SignupForm';
 import styles from '@/app/styles.module.css'
-
+import Image from 'next/image';
+import loginLogo from '../../../../public/images/others/cake logo.png'
 const SignUp = () => {
     return (
 
         <div className='lg:grid grid-cols-2 min-h-screen  mx-auto '>
             <div className={`hidden lg:flex justify-center items-center ${styles.bg_image}`}>
-                <div className='  text-white bg-[#FFFFFF26] backdrop-blur-lg p-20 text-5xl mx-20 relative'>
+                <div className=' fill rounded bg-opacity-80 bg-white backdrop-blur-lg p-20 mx-20 relative'>
 
 
-
-                    <h1 className=' text-white font-bold leading-snug'>Start for free today and get <strong className='text-black'>attractive</strong> offers.</h1>
-                    <p className='text-white text-sm leading-6'>Streamline operations with real-time metrics and user management.</p>
+                    <Image
+                        src={loginLogo}
+                        fil="true"
+                        alt="brand logo"
+                    />
+                    <p className=' text-md text-center '>Best photo retouching service provider</p>
                 </div>
             </div>
 
