23cbb3540ca7dcbdd6be996d0f4bc7db19370b33
premit01 <habibur.premit01@gmail.com>
2023-10-08 18:33:40 +0600

    chage image and add create new order

--- src/components/dashboard/DashboardHeader/DashboardHeader.js
+++ src/components/dashboard/DashboardHeader/DashboardHeader.js
@@ -4,7 +4,10 @@ import React, { useContext } from 'react';
 import { RxHamburgerMenu } from "react-icons/rx";
 const DashboardHeader = () => {
     const { isSidebarOpen, setSidebarOpen } = useContext(StateContext)
-
+    const newOrderHandler = () => {
+        window.location.reload()
+        window.location.href = "/dashboard/new_order";
+    }
 
     return (
         <div className='flex justify-between items-center py-5 '>
@@ -12,7 +15,7 @@ const DashboardHeader = () => {
                 <button onClick={() => setSidebarOpen(!isSidebarOpen)} className='text-xl mr-3 lg:hidden'> <RxHamburgerMenu /></button>
                 <h2 className='text-2xl font-bold'>New Order</h2>
             </div>
-            <button className='text-white bg-main px-3 py-2 rounded-lg'>Create new Order</button>
+            <button onClick={newOrderHandler} className='text-white bg-main px-3 py-2 rounded-lg'>Create new Order</button>
         </div>
     );
 };
