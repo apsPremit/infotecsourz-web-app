0b668c273fa9de78d6a2f2e7d7c57bef6d2971f4
habibur-pro <va.habibur@gmail.com>
2023-10-22 23:06:03 +0600

    calculate remaining balance

--- src/components/dashboard/dashboard/SubscribedPackage/SubscribedPackage.js
+++ src/components/dashboard/dashboard/SubscribedPackage/SubscribedPackage.js
@@ -4,6 +4,7 @@ import React from 'react';
 
 const SubscribedPackage = () => {
     const { userData } = UserAuth()
+    console.log('u', userData)
 
 
 
@@ -12,7 +13,11 @@ const SubscribedPackage = () => {
             <div className='mt-3 bg-blue-500 px-5 pt-2 pb-5 rounded-lg shadow'>
                 <p className='text-sm font-bold'>Subscribed Package</p>
                 <p className='text-lg font-bold my-1'>{userData?.subscribedPackage}</p>
-                <p className='text-sm'>Remaining Credit: <span className='font-bold'>{userData?.remainingCredit}</span></p>
+                {
+                    userData?.subscribedPackage === 'free trial' ?
+                        <p className='text-sm'>Remaining Balance: <span className='font-bold'>${userData?.remainingBalance?.toFixed(2)}</span></p>
+                        : <p className='text-sm'>Remaining Credit: <span className='font-bold'>{userData?.remainingCredit}</span></p>
+                }
             </div>
         </div>
     );
