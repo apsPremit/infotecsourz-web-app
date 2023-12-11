9954a30304a078bde01925e809d2d488c2e76b4a
apsPremit <147072483+apsPremit@users.noreply.github.com>
2023-12-11 17:57:32 +0600

    reload page when got erro

--- src/app/(dashboard)/dashboard/error.js
+++ src/app/(dashboard)/dashboard/error.js
@@ -1,9 +1,18 @@
 "use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Error({ error, reset }) {
+  const router = useRouter();
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-2xl font-bold">Something Went Wrong!</h1>
-      <button>Try Again</button>
+      <button
+        className="px-2 py-1.5 rounded bg-main text-white mt-5"
+        onClick={() => router.refresh()}
+      >
+        Try Again
+      </button>
     </div>
   );
 }
