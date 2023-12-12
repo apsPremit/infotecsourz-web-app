502e1ec3f17e3c0b5093473c402ebaeffa081bf7
apsPremit <147072483+apsPremit@users.noreply.github.com>
2023-12-12 12:46:37 +0600

    fix pricing isue

--- src/app/(dashboard)/dashboard/pricing/page.js
+++ src/app/(dashboard)/dashboard/pricing/page.js
@@ -1,27 +1,19 @@
 import React from "react";
 import SinglePackage from "@/components/dashboard/package/SinglePackage/SinglePackage";
 import { baseUrl } from "@/utils/functions/baseUrl";
+// import { baseUrl } from "@/utils/functions/baseUrl";
 
 export const metadata = {
   title: "Billing Info | Infotecsourz",
   description: "Photo Retouching App",
 };
 const Pricing = async () => {
-  const getAllPackage = async () => {
-    try {
-      const res = await fetch(`${baseUrl}/package`);
-      if (!res.ok) {
-        throw new Error(`${res.statusText}`);
-      }
-      const data = await res.json();
-      return data?.data;
-    } catch (error) {
-      console.log("pricing error is", error);
-      throw new Error(error.message || "Something went wrong");
-    }
-  };
-
-  const allPackage = await getAllPackage();
+  const res = await fetch(
+    `https://desktop-app-server-qnhin.ondigitalocean.app/api/v1/package`
+  );
+  const result = await res.json();
+  console.log(result);
+  const allPackage = result.data || [];
 
   return (
     <div>
@@ -29,11 +21,13 @@ const Pricing = async () => {
         Our exclusive subscription packages only made for you
       </h1>
       {/* <h1>{error.message}</h1> */}
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:px-10 mb-10">
-        {allPackage?.map((plan) => (
-          <SinglePackage key={plan._id} plan={plan} />
-        ))}
-      </div>
+      {allPackage?.length > 1 && (
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:px-10 mb-10">
+          {allPackage?.map((plan) => (
+            <SinglePackage key={plan._id} plan={plan} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
